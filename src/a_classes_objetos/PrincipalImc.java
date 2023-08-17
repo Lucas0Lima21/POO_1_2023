6212c8b8fc2833937fd64edca9c026757d2ecd09
Lucas0Lima21 <lucaslima1810202@gmail.com>
2023-08-17 19:24:08 -0300

    Projeto

---
+++ src/a_classes_objetos/PrincipalImc.java
@@ -0,0 +1,23 @@
+package a_classes_objetos;
+
+import javax.swing.JOptionPane;
+
+public class PrincipalImc {
+	public static void main(String[] args) {
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Informe a quantidade de pessoas"));
+		
+		Pessoa pessoas[]= new Pessoa[n];
+		
+		String resultado = "";
+		
+		for(int i=0; i<n; i++) {
+			Pessoa p = new Pessoa();
+			p.cadastra();
+			pessoas[i] = p;
+			resultado += p.exibirDados();
+		}
+		
+		JOptionPane.showMessageDialog(null, resultado);
+	}
+
+}
